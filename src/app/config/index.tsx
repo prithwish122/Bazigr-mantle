34118cb264a1a3690673b40ed0f7f19435d92247
Prithwish Chatterjee <prithwishchatterjee1277@gmail.com>
2026-01-15 20:55:32 +0530

    fixed build err

--- src/app/config/index.tsx
+++ src/app/config/index.tsx
@@ -48,7 +48,7 @@ const customNetwork = defineChain({
   rpcUrls: {
     default: {
       http: ['https://rpc.sepolia.mantle.xyz'],
-      // webSocket: ['wss://rpc-mainnet.u2u.xyz/ws'],
+      // webSocket:jj ['wss://rpc-mainnet.u2u.xyz/ws'],
     },
   },
   blockExplorers: {
